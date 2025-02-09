6db55c7eac9a62067fb068337042521f0517cc87
Brendo Damasceno <96849756+BrendoDamasceno@users.noreply.github.com>
2025-02-09 13:25:59 -0300

    Add files via upload

---
+++ Criando_comentarios_SQL.sql
@@ -0,0 +1,19 @@
+-- 1. Criar uma frase que explica o funcionamento de um determinado código 
+-- 2. Coementar apenas uma parte de um código e executar o restante
+-- 3. Comentar todo um trecho de um código de maneira rápida e otimizada
+
+--Este código seleciona as colunas de ID do produto e nome do produto da tabela de produto
+SELECT
+	--Productkey,
+	ProductName
+FROM
+	DimProduct
+
+/*SELECT
+	--Storekey,
+	StoreName,
+	StoreName
+FROM
+	DimStore*/
+
+SELECT * FROM DimProduct
